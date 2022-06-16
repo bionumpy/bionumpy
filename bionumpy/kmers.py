1b2948c320c2f5cc266767132a45f5668c8410cf
Ivar Grytten <ivar.grytten@gmail.com>
2022-06-16 10:25:20 +0200

    Merging

--- bionumpy/kmers.py
+++ bionumpy/kmers.py
@@ -10,35 +10,8 @@ logger = logging.getLogger(__name__)
 
 class KmerEncoding(RollableFunction):
 
-<<<<<<< HEAD
     def __init__(self, k, alphabet_encoding=None, alphabet_size=None):
         self.window_size = k
-=======
-    def hash(self, kmer):
-        return kmer.dot(self._powers)
-
-
-@convolution
-def fast_hash(sequence, k, do_encoding=True):
-    if do_encoding:
-        encoded = ACTGEncoding.from_bytes(sequence)
-    else:
-        encoded = sequence
-    bit_array = BitArray.pack(encoded, bit_stride=2)
-    hashes = bit_array.sliding_window(k)
-    return hashes
-
-
-def hash_sequences(sequences, k, hash_func):
-    sequence = sequences.ravel()
-    kmers = hash_func(sequence, k)
-    ragged_array = RaggedArray(kmers, sequences.shape)
-    return ragged_array[:, :-k+1]
-
-
-class KmerEncoding:
-    def __init__(self, k, alphabet_size=4):
->>>>>>> fast-hash-encoding-fix
         self._k = k
         self._encoding = alphabet_encoding
         if alphabet_encoding is not None:
