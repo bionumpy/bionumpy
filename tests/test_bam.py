64aa7a950d7b39d884044a1191c19588355e7dd6
Knut Rand <knutdrand@gmail.com>
2022-09-06 08:06:58 +0200

    Developing bam reader

--- tests/test_bam.py
+++ tests/test_bam.py
@@ -14,5 +14,9 @@ def test_read():
     chunk = np.frombuffer(f.read(300), dtype=np.uint8)
     buf = BamBuffer.from_raw_buffer(chunk)
     d = buf.get_data()
+    print(d)
     for a, b in zip(data["sam"], d):
-        assert a.position == b.position
+        assert b == a, (b, a)
+        # assert a.position == b.position
+        # assert a.name == b.name
+    assert False
