d1b98f7c5d9a66d3ff6ba9b12c7003e15c8d1cd3
Knut Rand <knutdrand@gmail.com>
2022-09-20 17:31:05 +0200

    Midstride, tests failing

--- bionumpy/sequences.py
+++ bionumpy/sequences.py
@@ -39,6 +39,29 @@ class EncodedArray(np.ndarray):
             return np.asarray(out_arr, dtype=bool)
         return out_arr
 
+    def __eq__(self, other):
+        if isinstance(other, int):
+            other = np.asanyarray(other)
+        else:
+            other = as_encoded_sequence_array(other, self.encoding)
+        print(self, other, np.asarray(self) == np.asarray(other))
+        return np.asarray(self) == np.asarray(other)
+
+#     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+#         print("HHHHHHHHHHHHEEEEEEEEEEEERRRRRRRRRRREEEEEEEEEEE")
+#         inputs = [np.asarray(as_encoded_sequence_array(i, self.encoding)) if isinstance(i, (str, list)) else np.asarray(i)
+#                   for i in inputs]
+#         print(inputs)
+#         ret = getattr(ufunc, method)(*inputs, **kwargs)
+#         ret = ret.view(self.__class__)
+#         ret.encoding = self.encoding
+#         return ret
+# 
+#         print(super())
+#         ret = super().__array_ufunc__(ufunc, method, *inputs, **kwargs)
+#         print(ret)
+#         return ret
+
 
 class NumericEncodedArray(EncodedArray):
     pass
@@ -54,7 +77,9 @@ def create_sequence_array_from_already_encoded_data(data: np.ndarray, encoding: 
     return Seqeunces(data._data, data.shape, encoding=encoding)
 
 def as_encoded_sequence_array(s, encoding: Encoding) -> EncodedArray:
+    print(s)
     s = as_sequence_array(s)
+
     if isinstance(s, RaggedArray):
         return s.__class__(as_encoded_sequence_array(s.ravel(), encoding), s.shape)
     if s.encoding != encoding:
@@ -69,12 +94,12 @@ def as_sequence_array(s) -> EncodedArray:#:, encoding=BaseEncoding):
     """
     Return a sequence array representation of s
     """
-    if isinstance(s, (Sequence)):
+    if isinstance(s, (EncodedArray)):
         return s
     elif isinstance(s, np.ndarray):
-        return s.view(Sequence)
+        return s.view(EncodedArray)
     elif isinstance(s, RaggedArray):
-        return Sequences(s._data, s.shape)
+        return RaggedArray(s._data.view(EncodedArray), s.shape)
     elif isinstance(s, str):
         return Sequence.from_string(s)
     elif isinstance(s, list):
