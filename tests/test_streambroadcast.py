ed951100e12d777dc850dc98edcab436b6ebde5c
Knut Rand <knutdrand@gmail.com>
2022-09-22 16:50:41 +0200

    Seems to work now wo keywords

--- tests/test_streambroadcast.py
+++ tests/test_streambroadcast.py
@@ -8,9 +8,10 @@ from bionumpy.code_nodes import ComputationNode, LeafNode, consume, Mockup
 class ExampleDatatype:
     a: int
     b: int
+    c: int
 
 
-all_data = ExampleDatatype(np.array([0, 1, 2, 3, 4]), np.array([2, 3, 4, 1, 0]))
+all_data = ExampleDatatype(np.array([0, 1, 2, 3, 4]), np.array([2, 3, 4, 1, 0]), np.array([100, 200, 1, 2, 3]))
 
 
 @pytest.fixture
@@ -23,15 +24,27 @@ def stream():
     return Mockup((c for c in (all_data[:2], all_data[2:4], all_data[4:])))
 
 
-def add(a, b):
+def add(a, b, c):
     return a+b
 
 
-def test_equal(whole_data, stream):
+def two_step(a, b, c):
+    return a*b+b-a
+
+
+def complicated(a, b, c):
+    d = a+b
+    e = b * c
+    f = c-a > 0
+    return np.where(f, np.exp(d+e+f), 1000)
+
+
+@pytest.mark.parametrize("func", [add, two_step, complicated])
+def test_equal(whole_data, stream, func):
     a = stream.a
     b = stream.b
     assert isinstance(a, LeafNode) and isinstance(b, LeafNode)
-    true = add(whole_data.a, whole_data.b)
-    temp = add(a, b)
+    true = func(whole_data.a, whole_data.b, whole_data.c)
+    temp = func(a, b, stream.c)
     ours = np.concatenate(consume(temp))
     assert np.all(true == ours)
