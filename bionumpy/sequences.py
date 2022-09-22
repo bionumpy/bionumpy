277fbf317ee06423d6d6afad49e82573db5bc16b
Knut Rand <knutdrand@gmail.com>
2022-09-22 16:03:23 +0200

    Midstridee nothing works

--- bionumpy/sequences.py
+++ bionumpy/sequences.py
@@ -7,12 +7,12 @@ class EncodedArray(np.ndarray):
     """ 
     Class for data that could be written as characters, but is represented numpy arrays
     """
-    
-    encoding = BaseEncoding
-
+    @classmethod
+    def encode(cls, byte_array):
+        return byte_array
 
     def decode(self):
-        return self.encoding.decode(self)
+        return self
 
     @classmethod
     def from_string(cls, s: str) -> "EncodedArray":
@@ -22,27 +22,16 @@ class EncodedArray(np.ndarray):
         return "".join(chr(n) for n in self)
 
     def __str__(self) -> str:
-        self = self.encoding.decode(self)
+        text = self.decode()
         if len(self.shape) == 1:
-            return '"' + "".join(chr(n) for n in self[:20]) + '"'
+            return '"' + "".join(chr(n) for n in text[:20]) + '"'
         a = np.array(["".join(chr(n) for n in seq[:20]) for seq in self.reshape(-1, self.shape[-1])]).reshape(self.shape[:-1])[:20]
         return str(a)
-
-    def __repr__(self) -> str:
-        return f"Sequence({np.asarray(self)}, {self.encoding.__class__.__name__})"
-
-    def __getitem__(self, key) -> "EncodedArray":
-        r = super().__getitem__(key)
-        if isinstance(r, (Sequence)):
-            r.encoding = self.encoding
-
-        return r
 
     def __array_function__(self, func, types, args, kwargs):
         if func == np.concatenate:
             return np.concatenate([np.asarray(e) for e in args[0]]).view(self.__class__)
         return super().__array_function__(func, types, args, kwargs)
-
 
     def __array_wrap__(self, out_arr, context=None):
         if out_arr.dtype == bool:
@@ -58,21 +47,12 @@ class EncodedArray(np.ndarray):
 
 
 class ASCIIText(EncodedArray):
-    @classmethod
-    def encode(cls, byte_array):
-        return byte_array
-
-    def decode(self):
-        return self
-
+    pass
 
 class Quality(EncodedArray):
     encoding = None
     def __str__(self):
         return "".join(chr(c) for c in self.decode())
-    
-    def __repr__(self):
-        return f"{self.__class__.__name__}({repr(np.asarray(self))})"
 
     @classmethod
     def encode(cls, byte_array):
@@ -90,7 +70,6 @@ class Quality(EncodedArray):
 #         return ret
 # 
 #         ret = super().__array_ufunc__(ufunc, method, *inputs, **kwargs)
-#         print(ret)
 #         return ret
 
 
@@ -108,25 +87,19 @@ def create_sequence_array_from_already_encoded_data(data: np.ndarray, encoding: 
     return Seqeunces(data._data, data.shape, encoding=encoding)
 
 def as_encoded_sequence_array(s, encoding: Encoding) -> EncodedArray:
-    print("<<<<<<<<<<<<<<", s, type(s))
     s = as_sequence_array(s)
 
     if isinstance(s, RaggedArray):
-        print("!!!!!!!!!!!!!!!!!!!!!", repr(s._data))
         return s.__class__(as_encoded_sequence_array(s.ravel(), encoding), s.shape)
-    print("AS ENCODED SEQUNCE ARRAY", s.encoding, encoding)
-    print(repr(s))
     if isinstance(encoding, type) and issubclass(encoding, EncodedArray):
         if isinstance(s, encoding):
             return s
         else:
             ret =  encoding.encode(s)
-            print("+++", repr(ret))
             return ret
                   
 
     if s.encoding != encoding:
-        print(s.encoding, encoding)
         e = encoding.encode(s)
         s = e.view(Sequence)
         s.encoding = encoding 
@@ -138,7 +111,6 @@ def as_sequence_array(s) -> EncodedArray:#:, encoding=BaseEncoding):
     """
     Return a sequence array representation of s
     """
-    print("??", repr(s), type(s))
     if isinstance(s, EncodedArray):
         return s
     elif isinstance(s, np.ndarray):
@@ -146,7 +118,6 @@ def as_sequence_array(s) -> EncodedArray:#:, encoding=BaseEncoding):
     elif isinstance(s, RaggedArray):
         return RaggedArray(as_sequence_array(s._data), s.shape)
     elif isinstance(s, str):
-        print(s, Sequence.from_string(s))
         return Sequence.from_string(s)
     elif isinstance(s, list):
         return RaggedArray(as_sequence_array("".join(s)), [len(ss) for ss in s])
