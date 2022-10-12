bac054c6276a087b618c09030ccfeb51575ce81f
Knut Rand <knutdrand@gmail.com>
2022-10-12 17:06:49 +0200

    Mutation signature works with count_encoded

--- bionumpy/mutation_signature.py
+++ bionumpy/mutation_signature.py
@@ -1,8 +1,9 @@
 import numpy as np
 from .encodings import ACTGEncoding
-from .sequences import as_encoded_sequence_array, as_sequence_array
-from .dna import complement
+from .sequences import as_encoded_sequence_array, as_sequence_array, EncodedArray
+from .dna import complement, reverse_compliment
 from .chromosome_map import ChromosomeMap
+from .counter import count_encoded
 from . import DNAArray 
 import logging
 
@@ -20,7 +21,7 @@ class SNPEncoding:
     lookup[int(as_encoded_sequence_array("T", DNAArray).ravel())][as_encoded_sequence_array("ACG", DNAArray)] = 3 + np.arange(3)
     lookup[int(as_encoded_sequence_array("A", DNAArray).ravel())][as_encoded_sequence_array("TGC", DNAArray)] = 3 + np.arange(3)
 
-    text = np.array([f"C>{c}" for c in "AGT"] + [f"T->{c}" for c in "ACG"])
+    text = np.array([f"C>{c}" for c in "AGT"] + [f"T>{c}" for c in "ACG"])
 
     @classmethod
     def to_string(cls, encoded):
@@ -35,7 +36,7 @@ class SNPEncoding:
         pass
 
 
-class MutationSignatureEncoding:
+class MutationTypeEncoding:
     def __init__(self, k, encoding=DNAArray):
         self.k = k
         self.h = 4 ** np.arange(k)
@@ -49,39 +50,49 @@ class MutationSignatureEncoding:
         kmer = np.asarray(kmer)
         kmer_hashes = np.dot(kmer, self.h)
         snp_hashes = SNPEncoding.encode(snp)
-        return kmer_hashes + 4 ** (self.k - 1) * snp_hashes
+        return (kmer_hashes + 4 ** (self.k - 1) * snp_hashes)
 
     def decode(self, encoded):
         snp = SNPEncoding.decode(encoded >> (2 * (self.k - 1)))
         chars = (encoded >> (2 * np.arange(self.k - 1))) & 3
-        kmer = "".join(chr(b) for b in ACTGEncoding.decode(chars))
+        kmer = "".join(chr(b) for b in self._encoding.encoding.decode(chars))
         return kmer[: self.k // 2] + "[" + snp + "]" + kmer[self.k // 2 :]
 
     def to_string(self, encoded):
         snp = SNPEncoding.to_string(encoded >> (2 * (self.k - 1)))
         chars = (encoded >> (2 * np.arange(self.k - 1))) & 3
-        kmer = "".join(chr(b) for b in ACTGEncoding.decode(chars))
+        kmer = "".join(chr(b) for b in self._encoding.encoding.decode(chars))
         return kmer[: self.k // 2] + "[" + snp + "]" + kmer[self.k // 2 :]
 
+    def get_labels(self):
+        return [self.to_string(c) for c in np.arange(4**(self.k-1)*6)]
+    
+def get_encoded_array_class(_encoding):
+    class MutationTypeArray(EncodedArray):
+        encoding=_encoding
+    f = _encoding.encode
+    _encoding.encode = lambda *args, **kwargs: f(*args, **kwargs).view(MutationTypeArray)
+
+    return MutationTypeArray
 
 @ChromosomeMap(reduction=sum)
-def get_kmers(snps, reference, flank):
+def count_mutation_types(snps, reference, flank=1):
     reference = as_encoded_sequence_array(reference, DNAArray)
-    snps.ref_seq = as_encoded_sequence_array(snps.ref_seq, DNAArray)
-    snps.alt_seq = as_encoded_sequence_array(snps.alt_seq, DNAArray)
+    snps.ref_seq = as_encoded_sequence_array(snps.ref_seq.ravel(), DNAArray)
+    snps.alt_seq = as_encoded_sequence_array(snps.alt_seq.ravel(), DNAArray)
     kmer_indexes = get_kmer_indexes(snps.position, flank=flank)
     kmers = reference[kmer_indexes]
     forward_mask = (snps.ref_seq == "C") | (snps.ref_seq == "T")
     kmers = np.where(
-        forward_mask[:, None], kmers, complement(kmers[:, ::-1])
+        forward_mask[:, None], kmers, reverse_compliment(kmers)
     ).view(DNAArray)
-    signature_encoding = MutationSignatureEncoding(flank * 2 + 1)
+    signature_encoding = MutationTypeEncoding(flank * 2 + 1)
+    get_encoded_array_class(signature_encoding)
     all_hashes = signature_encoding.encode(kmers, snps)
     n_hashes = 4 ** (flank * 2) * 6
-
+    
     if not hasattr(snps, "genotypes"):
-        counts = np.bincount(all_hashes, minlength=n_hashes)
-        return counts
+        return count_encoded(all_hashes)
     return np.array(
         [
             np.bincount(
--- scripts/mutation_signature_example.py
+++ scripts/mutation_signature_example.py
@@ -1,18 +1,10 @@
 import numpy as np
 import bionumpy as bnp
-from bionumpy.mutation_signature import (
-    get_kmers, MutationSignatureEncoding)
+from bionumpy.mutation_signature import count_mutation_types
 from bionumpy.util import get_snps
 from bionumpy.groupby import groupby
 import logging
 logging.basicConfig(level="INFO")
-
-
-def print_file(counts, flank):
-    print(",".join(MutationSignatureEncoding(2*flank+1).to_string(i)
-                   for i in range(counts.shape[-1])))
-    for row in np.atleast_2d(counts):
-        print(",".join(str(c) for c in row))
 
 
 def main(vcf_filename, fasta_filename, flank=1):
@@ -20,8 +12,8 @@ def main(vcf_filename, fasta_filename, flank=1):
     snps = get_snps(variants)
     snps = groupby(snps, "chromosome")
     reference = bnp.open(fasta_filename)
-    counts = get_kmers(snps, reference, flank)
-    print_file(counts, flank)
+    counts = count_mutation_types(snps, reference, flank)
+    print(counts)
 
 
 def test():
