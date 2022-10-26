82e48d1bbd66bf6f36335d2d8b162b04ba0d826b
Knut Rand <knutdrand@gmail.com>
2022-10-26 13:32:24 +0200

    Github action for doctest

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -31,7 +31,7 @@ import bionumpy
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx', 'sphinx.ext.autosummary', 'sphinx.ext.doctest']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx', 'sphinx.ext.autosummary', 'sphinx.ext.doctest', 'sphinx.ext.coverage']
 
 doctest_global_setup = """
 import os
