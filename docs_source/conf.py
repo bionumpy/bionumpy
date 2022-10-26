813df75d5c310c5237c789bea0df4185f2b91e2f
Knut Rand <knutdrand@gmail.com>
2022-10-26 12:20:36 +0200

    Update conf for doctest and fastqc_example passing

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -31,8 +31,12 @@ import bionumpy
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx', 'sphinx.ext.autosummary', 'sphinx.ext.doctest']
 
+doctest_global_setup = """
+import os
+os.chdir("../")
+"""
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
