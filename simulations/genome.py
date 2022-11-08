2d7ff0a53cdf316e9533ee61d0d2e781f240e5c6
Knut Rand <knutdrand@gmail.com>
2022-11-08 15:03:17 +0100

    Update simulate

--- simulations/genome.py
+++ simulations/genome.py
@@ -1,9 +1,8 @@
 import bionumpy as bnp
 import pyfaidx
 from bionumpy.encodings.alphabet_encoding import DNAEncoding
-from bionumpy.sequences import create_sequence_array_from_already_encoded_data, to_ascii
-from bionumpy.datatypes import SequenceEntry, Variant, Interval
-from npstructures import RaggedArray
+from bionumpy.encoded_array import EncodedArray, EncodedRaggedArray
+from bionumpy.datatypes import SequenceEntry, Variant, Interval, VCFEntry
 import numpy as np
 
 np.random.seed(4321)
@@ -11,8 +10,8 @@ np.random.seed(4321)
 
 def simulate_genome(chrom_sizes):
     letters = np.random.randint(0, 4, size=sum(chrom_sizes))
-    letters = DNAEncoding.decode(create_sequence_array_from_already_encoded_data(letters, DNAEncoding))
-    sequences = RaggedArray(letters, chrom_sizes)
+    letters = EncodedArray(letters, DNAEncoding)
+    sequences = EncodedRaggedArray(letters, chrom_sizes)
     return SequenceEntry([str(i) for i, _ in enumerate(chrom_sizes)], sequences)
 
 
@@ -20,9 +19,13 @@ def simulate_snps(bases, chrom_name, rate=0.05):
     # bases = sequence_entries.sequence.ravel()
     positions = np.flatnonzero(np.random.choice([0, 1], size=bases.size, p=[1-rate, rate]))
     ref_seq = bases[positions]
-    alt_seq = DNAEncoding.decode(np.random.randint(0, 4, size=ref_seq.size))
+    alt_seq = EncodedArray(np.random.randint(0, 4, size=ref_seq.size), DNAEncoding)
+    print(ref_seq, alt_seq)
     mask = ref_seq != alt_seq
-    return Variant([chrom_name]*mask.sum(), positions[mask], ref_seq[mask], alt_seq[mask])
+    print(mask)
+    return VCFEntry([chrom_name]*int(mask.sum()), positions[mask], ["."]*int(mask.sum()),
+                    EncodedRaggedArray(ref_seq[mask], np.ones(ref_seq[mask].size, dtype=int)),
+                    EncodedRaggedArray(alt_seq[mask], np.ones(alt_seq[mask].size, dtype=int)))
 
 
 def simulate_intervals(chrom_name, chrom_size):
@@ -44,5 +47,6 @@ intervals = np.concatenate(
 
 bnp.open("example_data/small_genome.fa", "w").write(entries)
 pyfaidx.Faidx("example_data/small_genome.fa")
+print(snps)
 bnp.open("example_data/few_variants.vcf", "w").write(snps)
 bnp.open("example_data/small_interval.bed", "w").write(intervals)
