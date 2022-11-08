eb0eb34536eaebd1ac67032999e92af16ca50444
Knut Rand <knutdrand@gmail.com>
2022-11-08 13:06:22 +0100

    Set dtype in encodedarray explicitly. Fix Issue #21

--- tests/test_encoded_array.py
+++ tests/test_encoded_array.py
@@ -1,5 +1,6 @@
 import pytest
-from bionumpy.encoded_array import EncodedArray, EncodedRaggedArray
+from bionumpy.encoded_array import EncodedArray, EncodedRaggedArray, as_encoded_array
+from bionumpy.testing import assert_encoded_raggedarray_equal
 import bionumpy as bnp
 import numpy as np
 
@@ -37,6 +38,23 @@ def test_works_with_ragged():
     encoded_array = EncodedArray([67, 68, 69], bnp.encodings.BaseEncoding)
     encoded_array == "D"
     ragged = EncodedRaggedArray(encoded_array, [2, 1])
+
+
+def test_empty_encoded():
+    result = as_encoded_array(['', ''])
+    true = EncodedRaggedArray(EncodedArray(np.empty((0,), dtype=np.uint8)),
+                              [0, 0])
+                                           
+    assert_encoded_raggedarray_equal(result, true)
+
+
+def test_empty_dna_encoded():
+    result = as_encoded_array(['', ''], bnp.DNAEncoding)
+    true = EncodedRaggedArray(EncodedArray(np.empty((0,), dtype=np.uint8), bnp.DNAEncoding),
+                              [0, 0])
+                                           
+    assert_encoded_raggedarray_equal(result, true)
+    
 
 
 if __name__ == "__main__":
