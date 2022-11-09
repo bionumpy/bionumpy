dcac6728915327f17ecc2388b9475616f0f202bf
Knut Rand <knutdrand@gmail.com>
2022-11-09 11:16:22 +0100

    Documenting multistream sort order stuff. Moved groupby to streams

--- bionumpy/streams/__init__.py
+++ bionumpy/streams/__init__.py
@@ -2,8 +2,9 @@ from .stream import BnpStream, NpDataclassStream
 from .decorators import streamable
 from .reductions import mean, bincount, histogram, quantile
 from .grouped import grouped_dict, grouped_stream
+from .groupby_func import groupby
 from .multistream import MultiStream
 
 __all__ = ["BnpStream", "streamable", "decorators",
            "mean", "bincount", "histogram", "quantile", "NpDataclassStream",
-           "BnpStream", "MultiStream"]
+           "BnpStream", "MultiStream", "groupby"]
