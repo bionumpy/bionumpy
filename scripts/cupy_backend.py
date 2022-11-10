bd83b18eee825325e4b965cc557e8fbaef8f641f
Ivar Grytten <ivar.grytten@gmail.com>
2022-11-10 15:01:08 +0100

    Cleaned up cupy test

--- scripts/cupy_backend.py
+++ scripts/cupy_backend.py
@@ -1,18 +1,21 @@
 import numpy as np
 import cupy as cp
-
+import sys
 import bionumpy as bnp
 from bionumpy.io.file_buffers import TwoLineFastaBuffer
 
 bnp.set_backend(cp)
 
-fasta_filename = "example_data/small.fa"
-chunk_size = 5000000
 
-chunk_generator = bnp.open(fasta_filename, buffer_type=TwoLineFastaBuffer).read_chunks(min_chunk_size=chunk_size)
-for chunk, _ in zip(chunk_generator, range(5)):
-    encoded_sequence = bnp.as_encoded_array(chunk.sequence, bnp.encodings.alphabet_encoding.ACTGEncoding)
-    kmers = bnp.kmers.fast_hash(encoded_sequence, 31, bnp.encodings.alphabet_encoding.ACTGEncoding).ravel()
-    print(type(kmers))
-    print(kmers)
+def run(fasta_filename="example_data/small.fa"):
+    chunk_size = 500000
+    chunk_generator = bnp.open(fasta_filename, buffer_type=TwoLineFastaBuffer).read_chunks(min_chunk_size=chunk_size)
+    for chunk, _ in zip(chunk_generator, range(5)):
+        encoded_sequence = bnp.as_encoded_array(chunk.sequence, bnp.encodings.alphabet_encoding.ACTGEncoding)
+        kmers = bnp.kmers.fast_hash(encoded_sequence, 31, bnp.encodings.alphabet_encoding.ACTGEncoding).ravel()
+        print(type(kmers))
+        print(kmers)
 
+
+if __name__ == "__main__":
+    run(sys.argv[1])
