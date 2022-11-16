38c164cb7106a9b32a7ae82fb54afc571bda07c1
kanduric <chakri.co@gmail.com>
2022-11-16 14:49:27 +0100

    wildcard lookup

--- tests/test_wildcard_index.py
+++ tests/test_wildcard_index.py
@@ -1,7 +1,10 @@
 import pytest
 import bionumpy as bnp
 import numpy as np
-from bionumpy.wildcard_index import WildCardIndex
+
+from bionumpy import as_encoded_array
+from bionumpy.wildcard_index import WildCardIndex, WildCardLookup
+from bionumpy.testing import assert_encoded_raggedarray_equal
 
 
 @pytest.fixture
@@ -11,10 +14,16 @@ def sequences():
 
 def test_wildcard_index(sequences):
     index = WildCardIndex.create_index(sequences)
-    pattern_indices = index.get_pattern_indices(pattern="A.G")
-    np.testing.assert_equal(pattern_indices, [0])
-    np.testing.assert_equal(index.get_pattern_indices("CGT"), [0, 1])
-    np.testing.assert_equal(index.get_pattern_indices("ACG"), [0])
-    np.testing.assert_equal(index.get_pattern_indices("ACGCG"), [])
+    np.testing.assert_equal(index.get_indices(pattern="A.G"), [0])
+    np.testing.assert_equal(index.get_indices("CGT"), [0, 1])
+    np.testing.assert_equal(index.get_indices("ACG"), [0])
+    np.testing.assert_equal(index.get_indices("ACGCG"), [])
+    np.testing.assert_equal(index.get_indices("A..TA"), [0])
+
+
+def test_wildcard_lookup(sequences):
+    lookup = WildCardLookup.create_lookup(sequences)
+    assert lookup.get_sequences("TA.") == ["ACGTACG", "CGTAGT"]
+    assert len(lookup.get_sequences("GC.")) == 0
 
 
