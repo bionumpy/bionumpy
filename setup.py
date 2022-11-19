8eff8e82fce42fdab3543cea1090147d69fdd577
BumpVersion Action <bumpversion@github-actions>
2022-11-19 13:33:41 +0000

    Bump version: 0.2.5 → 0.2.6

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/knutdrand/bionumpy',
-    version='0.2.5',
+    version='0.2.6',
     zip_safe=False,
 )
 
