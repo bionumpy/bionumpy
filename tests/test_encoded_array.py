300cd2ee5e6f6fc6a9650c22d7486df2c6cae583
kanduric <chakri.co@gmail.com>
2022-11-21 14:20:12 +0100

    adding array functions to encoded array

--- tests/test_encoded_array.py
+++ tests/test_encoded_array.py
@@ -1,6 +1,6 @@
 import pytest
 from bionumpy.encoded_array import EncodedArray, EncodedRaggedArray, as_encoded_array
-from bionumpy.testing import assert_encoded_raggedarray_equal
+from bionumpy.testing import assert_encoded_raggedarray_equal, assert_encoded_array_equal
 import bionumpy as bnp
 import numpy as np
 
@@ -8,6 +8,10 @@ import numpy as np
 @pytest.fixture
 def sequence():
     return EncodedArray(np.array([[1, 2], [2, 3]], dtype=np.uint8)+64, bnp.encodings.BaseEncoding)
+
+@pytest.fixture
+def simple_sequence():
+    return EncodedArray(np.array([1, 2, 2, 3], dtype=np.uint8) + 64, bnp.encodings.BaseEncoding)
 
 
 def _test_sequence_repr(sequence):
@@ -54,7 +58,16 @@ def test_empty_dna_encoded():
                               [0, 0])
                                            
     assert_encoded_raggedarray_equal(result, true)
-    
+
+def test_array_function(simple_sequence):
+    result = np.append(simple_sequence, simple_sequence)
+    concat_sequence = EncodedArray(np.array([1, 2, 2, 3, 1, 2, 2, 3], dtype=np.uint8) + 64, bnp.encodings.BaseEncoding)
+    assert_encoded_array_equal(result,  concat_sequence)
+
+def test_insert_function(simple_sequence):
+    result = np.insert(simple_sequence, 0, simple_sequence)
+    concat_sequence = EncodedArray(np.array([1, 2, 2, 3, 1, 2, 2, 3], dtype=np.uint8) + 64, bnp.encodings.BaseEncoding)
+    assert_encoded_array_equal(result, concat_sequence)
 
 
 if __name__ == "__main__":
