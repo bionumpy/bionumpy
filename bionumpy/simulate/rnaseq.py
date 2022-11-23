6a6ec7de023378934d6b94e65c04da7fb1582aa6
kanduric <chakri.co@gmail.com>
2022-11-23 15:22:46 +0100

    simulate RNAseq

--- bionumpy/simulate/rnaseq.py
+++ bionumpy/simulate/rnaseq.py
@@ -1,13 +1,52 @@
+import dataclasses
 from itertools import chain
+import numpy as np
+from numpy.random import default_rng
+from bionumpy import EncodedArray, as_encoded_array
+from bionumpy.dna import reverse_compliment
+from bionumpy.encodings import StrandEncoding
+
+rng = default_rng()
+
+
+@dataclasses.dataclass
+class RNASeqSimulationSettings:
+    transcript_counts: list = None
+    fragment_size: int = 100
+    sampling_rate: float = 0.9
+    read_length: int = 75
+
 
 def get_transcript_copies(sequences, sequence_counts):
+    if sequence_counts is None:
+        sequence_counts = [1] * len(sequences)
     indices = list(chain(*[[i] * count for i, count in enumerate(sequence_counts)]))
     return sequences[indices]
 
-def fragment_transcript_copies(sequences, fragment_size):
-    fragments = []
-    for sequence in sequences:
-        for i in range(0, len(sequence) - fragment_size + 1, fragment_size):
-            fragments.append(sequence[i:i + fragment_size])
-    return fragments
 
+def fragment_transcript_copies(sequences, fragment_size):
+    fragments = [sequence[i:i + fragment_size]
+                 for sequence in sequences
+                 for i in range(0, len(sequence) - fragment_size + 1, fragment_size)]
+    return as_encoded_array(fragments, sequences.encoding)
+
+
+def sample_transcript_fragments(sequences, sampling_rate):
+    mask = np.random.choice(a=[True, False], size=len(sequences), p=[sampling_rate, 1 - sampling_rate])
+    return sequences[mask]
+
+
+def get_rnaseq_reads(fragments, read_length, strands=None):
+    reverse_fragments = reverse_compliment(fragments)
+    if strands is None:
+        strands = EncodedArray(rng.choice([0, 1], replace=True, size=len(fragments)), StrandEncoding)
+    reads = np.where(strands[:, np.newaxis] == "+", fragments[:, 0:read_length], reverse_fragments[:, 0:read_length])
+    return reads
+
+
+def simulate_rnaseq(reference_sequences, settings: RNASeqSimulationSettings):
+    transcript_copies = get_transcript_copies(reference_sequences, settings.transcript_counts)
+    fragmented_transcript_copies = fragment_transcript_copies(transcript_copies, settings.fragment_size)
+    sampled_transcript_fragments = sample_transcript_fragments(fragmented_transcript_copies, settings.sampling_rate)
+    rnaseq_reads = get_rnaseq_reads(sampled_transcript_fragments, settings.read_length)
+    return rnaseq_reads
--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,11 +1,18 @@
 import pytest
+
+from bionumpy import EncodedArray, as_encoded_array
+from bionumpy.dna import reverse_compliment
+from bionumpy.encodings import StrandEncoding
 from bionumpy.io.motifs import Motif, read_motif
 from bionumpy.simulate.chipseq import simulate_chip_seq_fragments, simulate_read_fragments
-from bionumpy.simulate.rnaseq import get_transcript_copies, fragment_transcript_copies
-from bionumpy.sequence import get_kmers
+from bionumpy.simulate.rnaseq import get_transcript_copies, fragment_transcript_copies, sample_transcript_fragments, \
+    get_rnaseq_reads, RNASeqSimulationSettings, simulate_rnaseq
 import bionumpy as bnp
 import numpy as np
 from itertools import chain
+from numpy.random import default_rng
+
+rng = default_rng()
 
 
 @pytest.fixture
@@ -28,18 +35,29 @@ def test_simulate_chipseq(sequence, motif):
     reads = simulate_read_fragments(fragments, 5)
     assert isinstance(reads, bnp.datatypes.Interval)
 
+
 @pytest.fixture
 def sequences():
     return bnp.as_encoded_array(["ACGT", "GCTA", "GTAAAT"], bnp.DNAEncoding)
 
+
 @pytest.fixture
 def sequence_counts():
-    return [2,1,3]
+    return [2, 1, 3]
+
+
+@pytest.fixture
+def rnaseq_simulation_settings(sequence_counts):
+    return RNASeqSimulationSettings(fragment_size=3, read_length=2, transcript_counts=sequence_counts)
+
 
 def test_get_transcript_copies(sequences, sequence_counts):
-    truth = bnp.as_encoded_array(list(chain(*[[sequence.to_string()]*count for sequence,count in zip(sequences, sequence_counts)])), bnp.DNAEncoding)
+    truth = bnp.as_encoded_array(
+        list(chain(*[[sequence.to_string()] * count for sequence, count in zip(sequences, sequence_counts)])),
+        bnp.DNAEncoding)
     result = get_transcript_copies(sequences, sequence_counts)
-    bnp.testing.assert_encoded_raggedarray_equal(truth,result)
+    bnp.testing.assert_encoded_raggedarray_equal(truth, result)
+
 
 def test_fragment_transcript_copies(sequences, fragment_size=2):
     truth = []
@@ -47,8 +65,29 @@ def test_fragment_transcript_copies(sequences, fragment_size=2):
         for i in range(0, len(sequence) - fragment_size + 1, fragment_size):
             truth.append(sequence[i:i + fragment_size])
     result = fragment_transcript_copies(sequences, fragment_size)
-    bnp.testing.assert_encoded_raggedarray_equal(truth,result)
+    bnp.testing.assert_encoded_raggedarray_equal(bnp.as_encoded_array(truth, sequences.encoding), bnp.as_encoded_array(result, sequences.encoding))
+
 
 def test_sample_transcript_fragments(sequences, sampling_rate=0.9):
-    pass
+    np.random.seed(seed=123)
+    mask = np.random.choice(a=[True, False], size=len(sequences), p=[sampling_rate, 1 - sampling_rate])
+    truth = sequences[mask]
+    result = sample_transcript_fragments(sequences, sampling_rate=0.9)
+    bnp.testing.assert_encoded_raggedarray_equal(bnp.as_encoded_array(truth), bnp.as_encoded_array(result))
+
+
+def test_get_rnaseq_reads(sequences, read_length=3):
+    pos_strands = as_encoded_array("+" * len(sequences), StrandEncoding)
+    pos_truth = as_encoded_array(["ACG", "GCT", "GTA"], sequences.encoding)
+    neg_strands = as_encoded_array("-" * len(sequences), StrandEncoding)
+    neg_truth = as_encoded_array(["ACG", "TAG", "ATT"], sequences.encoding)
+    pos_result = get_rnaseq_reads(sequences, read_length, strands=pos_strands)
+    neg_result = get_rnaseq_reads(sequences, read_length, strands=neg_strands)
+    bnp.testing.assert_encoded_raggedarray_equal(pos_truth, pos_result)
+    bnp.testing.assert_encoded_raggedarray_equal(neg_truth, neg_result)
+
+
+def test_simualte_rnaseq(sequences, rnaseq_simulation_settings):
+    result = simulate_rnaseq(sequences, rnaseq_simulation_settings)
+    assert np.all([len(item) for item in result] == [rnaseq_simulation_settings.read_length] * len(result))
 
