5168ab4d8e365f34ce3a01e1fe92eec181513d35
kanduric <chakri.co@gmail.com>
2022-11-23 16:03:51 +0100

    kmer_nmf abundance estimation

--- scripts/kmer_nmf.py
+++ scripts/kmer_nmf.py
@@ -1,20 +1,24 @@
 import scipy.stats
-
-from bionumpy import EncodedRaggedArray, count_encoded
+import bionumpy as bnp
+from bionumpy import EncodedRaggedArray, count_encoded, as_encoded_array
+from bionumpy.dna import reverse_compliment
 from bionumpy.sequence import get_kmers
+from bionumpy.simulate import rnaseq
 from sklearn.decomposition import NMF
 from scipy.optimize import minimize
 import numpy as np
+import plotly.express as px
 
 
 def estimate_reference_proportions(reference_sequences: EncodedRaggedArray, query_sequences: EncodedRaggedArray):
     reference_kmers = get_kmers(reference_sequences, k=3)
     query_kmers = get_kmers(query_sequences, k=3)
-    reference_counts = count_encoded(reference_kmers)
+    reference_counts = count_encoded(reference_kmers) + count_encoded(get_kmers(reverse_compliment(reference_sequences), k=3))
     query_counts = count_encoded(query_kmers, axis=None)
+    print("reference_counts", reference_counts.counts)
+    print("query_counts", query_counts.counts)
     loss_func = get_loss_func(reference_counts.counts, query_counts.counts)
-    print(minimize(loss_func, x0=np.ones(len(reference_sequences))))
-    return minimize(loss_func, x0=np.ones(len(reference_sequences))).x ** 2
+    return minimize(loss_func, x0=np.ones(len(reference_sequences))).x ** 2 * 2 * (reference_sequences.shape.lengths - 2)
 
 
 # def loss(k, rate):
@@ -32,3 +36,18 @@ def get_loss_func(reference_counts, query_counts):
         return -np.sum(pmf)
 
     return poisson_loss
+
+
+def _test():
+    transcriptome = as_encoded_array(["ACGT", "GGGATC", "AAATCG", "TTTTACG"], bnp.DNAEncoding)
+    rnaseq_reads = rnaseq.simulate_rnaseq(transcriptome,
+                                          rnaseq.RNASeqSimulationSettings(fragment_size=4, read_length=3,
+                                                                          transcript_counts=[9, 18, 27, 36]))
+    estimated_reads = estimate_reference_proportions(transcriptome, rnaseq_reads)
+    return estimated_reads
+
+
+if __name__ == '__main__':
+    est_reads = _test()
+    fig = px.scatter(x=est_reads, y=[9, 18, 27, 36])
+    fig.show()
