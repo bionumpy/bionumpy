afc453d4f746046e5a89dee7ef57fc7223ddf8ed
Knut Rand <knutdrand@gmail.com>
2022-11-24 13:03:41 +0100

    Fixed union in typing

--- bionumpy/typing.py
+++ bionumpy/typing.py
@@ -1,6 +1,6 @@
 from typing import List, Union
 from .encoded_array import EncodedArray, EncodedRaggedArray
 
-SingleEncodedArrayLike = Union(str, EncodedArray)
-EncodedRaggedArrayLike = Union(List[str], EncodedRaggedArray, List[EncodedArray])
-EncodedArrayLike = Union(SingleEncodedArrayLike, EncodedRaggedArrayLike)
+SingleEncodedArrayLike = Union[str, EncodedArray]
+EncodedRaggedArrayLike = Union[List[str], EncodedRaggedArray, List[EncodedArray]]
+EncodedArrayLike = Union[SingleEncodedArrayLike, EncodedRaggedArrayLike]
