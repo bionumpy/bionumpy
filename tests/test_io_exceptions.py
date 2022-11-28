a40fb5127fcc6c0bb4b70eecc39465d768ef74f7
Knut Rand <knutdrand@gmail.com>
2022-11-28 14:35:43 +0100

    Line numbers

--- tests/test_io_exceptions.py
+++ tests/test_io_exceptions.py
@@ -28,23 +28,23 @@ actg
 +
 """, 4)]
 
-malformed_two_line_fastas = ["""\
+malformed_two_line_fastas = [("""\
 >header
 acggtt
 acggtt
 >header
 acgtt
-"""]
+""", 2)]
 
 
-malformed_bed_files = ["""\
+malformed_bed_files = [("""\
 chr1\t10\t20
 chr2\t10\ttwenty
-""",
-                       """\
+""", 1),
+                       ("""\
 chr1\t10\t20
 chr2\t10\t20\t30
-"""]
+""", 1)]
 
 
 @pytest.mark.parametrize("data", malformed_fastqs)
@@ -57,17 +57,21 @@ def test_fastq_raises_format_exception(data):
     assert e.value.line_number == error_line
 
 
-@pytest.mark.parametrize("text", malformed_two_line_fastas)
-def test_fasta_raises_format_exception(text):
+@pytest.mark.parametrize("data", malformed_two_line_fastas)
+def test_fasta_raises_format_exception(data):
+    text, error_line = data
     buf_type = TwoLineFastaBuffer
-    with pytest.raises(FormatException):
+    with pytest.raises(FormatException) as e:
         buf = buf_type.from_raw_buffer(bnp.as_encoded_array(text))
         buf.get_data()
+    assert e.value.line_number == error_line
 
 
-@pytest.mark.parametrize("text", malformed_bed_files)
-def test_bed_raises_format_exception(text):
+@pytest.mark.parametrize("data", malformed_bed_files)
+def test_bed_raises_format_exception(data):
+    text, error_line = data
     buf_type = BedBuffer
-    with pytest.raises(FormatException):
+    with pytest.raises(FormatException) as e:
         buf = buf_type.from_raw_buffer(bnp.as_encoded_array(text))
         buf.get_data()
+    # assert e.value.line_number == error_line
