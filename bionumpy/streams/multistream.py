df06a5cd1c288a13e79cd428971904c005707673
Ivar Grytten <ivar.grytten@gmail.com>
2022-11-30 16:22:52 +0100

    Benchmark dev. Changed Multistream to work with ChromosomeSize

--- bionumpy/streams/multistream.py
+++ bionumpy/streams/multistream.py
@@ -1,7 +1,12 @@
+from typing import Union
+
 from .stream import BnpStream
 from . import groupby
 import logging
 import sys
+
+from ..datatypes import ChromosomeSize
+
 logger = logging.getLogger(__name__)
 
 
@@ -151,7 +156,7 @@ class MultiStream:
                    chromosome                    start                     stop
                          chr2                        2                       13
     """
-    def __init__(self, sequence_sizes: SequenceSizes, **kwargs):
+    def __init__(self, sequence_sizes: Union[SequenceSizes, ChromosomeSize], **kwargs):
         """Synch different data streams with data on the same reference
     
         Take streams or dict-like objects and return a stream that gives
@@ -167,13 +172,23 @@ class MultiStream:
         --------
         FIXME: Add docs.
         """
+        if isinstance(sequence_sizes, dict):
+            sequence_names = list(sequence_sizes.keys())
+            sequence_lengths = list(sequence_sizes.values())
+        elif isinstance(sequence_sizes, ChromosomeSize):
+            sequence_names = sequence_sizes.name.tolist()
+            sequence_lengths = sequence_sizes.size.tolist()
+        else:
+            raise Exception("MultiStream needs sequence_sizes as either a "
+                            "dict-like object or a ChromosomeSize object, not %s" % sequence_sizes)
+
         self._streams = {}
-        self.lengths = BnpStream(sequence_sizes.values())
+        self.lengths = BnpStream(sequence_lengths)
         for keyword, value in kwargs.items():
             if isinstance(value, BnpStream):
-                self.__dict__[keyword] = SynchedStream(value, list(sequence_sizes.keys()))
+                self.__dict__[keyword] = SynchedStream(value, sequence_names)
             elif hasattr(value, "__getitem__"):
-                self.__dict__[keyword] = IndexedStream(value, list(sequence_sizes.keys()))
+                self.__dict__[keyword] = IndexedStream(value, sequence_names)
             else:
                 raise ValueError(f"Only streams and dict-like objects can be used in MultiStream. {keyword}:{value}")
 
