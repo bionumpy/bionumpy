297390af93a73fad53ef088353eb1a073ecb55f3
Knut Rand <knutdrand@gmail.com>
2022-12-06 14:42:07 +0100

    Document open_indexed

--- bionumpy/io/indexed_files.py
+++ bionumpy/io/indexed_files.py
@@ -11,7 +11,36 @@ class IndexBuffer(DelimitedBuffer):
     dataclass = FastaIdx
 
 
-def open_indexed(filename):
+def open_indexed(filename: str) -> IndexedFasta:
+    """Open an indexed fasta (for now) file with random access
+
+    If an index is not already present for the file, create it
+
+    Parameters
+    ----------
+    filename : str
+        The filename of the file
+
+    Returns
+    -------
+    IndexedFasta
+        An Indexed fasta object that supports random access on
+        chromosome or intervals
+
+    Examples
+    --------
+    >>> reference = open_indexed("example_data/small_genome.fa")
+    >>> reference
+    Indexed Fasta File with chromosome sizes: {'0': 80, '1': 80, '2': 80, '3': 80}
+    >>> reference["1"]
+    encoded_array('gcttggtatgaaaacccatc...')
+    >>> from bionumpy.datatypes import Interval
+    >>> intervals = Interval.from_entry_tuples([("1", 10, 20), ("2", 20, 30)])
+    >>> reference.get_interval_sequences(intervals)
+    encoded_ragged_array(['aaaacccatc',
+                          'ggccgttttt'])
+    """
+
     path = PurePath(filename)
     suffix = path.suffixes[-1]
     index_file_name = path.with_suffix(path.suffix + ".fai")
