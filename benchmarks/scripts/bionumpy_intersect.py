10f1daeabfd83d60ae4919799cd726a885fc9c62
Knut Rand <knutdrand@gmail.com>
2022-12-15 15:12:34 +0100

    midstide

--- benchmarks/scripts/bionumpy_intersect.py
+++ benchmarks/scripts/bionumpy_intersect.py
@@ -11,11 +11,11 @@ chromosome_encoding = bnp.encodings.string_encodings.StringEncoding(chrom_sizes.
 a = dataclasses.replace(a, chromosome=chromosome_encoding.encode(a.chromosome))
 b = dataclasses.replace(b, chromosome=chromosome_encoding.encode(b.chromosome))
 
-a = bnp.arithmetics.sort_intervals(a, sort_order=chrom_sizes.name.tolist())
-b = bnp.arithmetics.sort_intervals(b, sort_order=chrom_sizes.name.tolist())
+# a = bnp.arithmetics.sort_intervals(a, sort_order=chrom_sizes.name.tolist())
+# b = bnp.arithmetics.sort_intervals(b, sort_order=chrom_sizes.name.tolist())
 print("DOne sorting")
 
 #ms = bnp.streams.MultiStream(chrom_sizes, a=a, b=b)
-result = bnp.arithmetics.intersect(a, b)
+result = bnp.arithmetics.global_intersect(a, b)
 with bnp.open(snakemake.output[0], "w", buffer_type=bnp.Bed6Buffer) as f:
     f.write(result)
