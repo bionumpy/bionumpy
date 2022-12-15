5295ed8e7329e849184bfd145c3cb027a293df10
Knut Rand <knutdrand@gmail.com>
2022-12-15 12:00:46 +0100

    gloabel intersect in artih

--- bionumpy/arithmetics/__init__.py
+++ bionumpy/arithmetics/__init__.py
@@ -1,3 +1,3 @@
-from .intervals import (count_overlap, get_boolean_mask,
+from .intervals import (count_overlap, get_boolean_mask, gloabal_intersect,
                         get_pileup, intersect, sort_intervals, merge_intervals)
 from .similarity_measures import forbes, jaccard
