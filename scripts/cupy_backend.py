4dbdd1c83842c78d99bd8875ee052e2a878071a5
Ivar Grytten <ivar.grytten@gmail.com>
2022-12-19 11:19:33 +0100

    started updating cupy example

--- scripts/cupy_backend.py
+++ scripts/cupy_backend.py
@@ -2,7 +2,6 @@ import cupy as cp
 import sys
 import bionumpy as bnp
 import bionumpy.encoded_array
-import bionumpy.encoded_array_functions
 from bionumpy.io.file_buffers import TwoLineFastaBuffer
 
 bnp.set_backend(cp)
@@ -12,8 +11,9 @@ def run(fasta_filename="example_data/small.fa"):
     chunk_size = 500000
     chunk_generator = bnp.open(fasta_filename, buffer_type=TwoLineFastaBuffer).read_chunks(min_chunk_size=chunk_size)
     for chunk, _ in zip(chunk_generator, range(5)):
-        encoded_sequence = bionumpy.encoded_array.as_encoded_array(chunk.sequence, bnp.encodings.alphabet_encoding.ACTGEncoding)
-        kmers = bionumpy.sequence.kmers.get_kmers(encoded_sequence, 31).ravel()
+        #encoded_sequence = bionumpy.encoded_array.as_encoded_array(chunk.sequence, bnp.encodings.alphabet_encoding.ACTGEncoding)
+        s = bnp.change_encoding(chunk.sequence, bnp.DNAEncoding)
+        kmers = bionumpy.sequence.kmers.get_kmers(s, 31).ravel()
         print(type(kmers))
         print(kmers)
 
