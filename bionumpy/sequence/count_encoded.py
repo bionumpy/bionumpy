c120c2e02d88e9ea66b085a0d4d3ad648e03da54
Knut Rand <knutdrand@gmail.com>
2023-01-06 15:11:08 +0100

    most_common on EncodedCounts

--- bionumpy/sequence/count_encoded.py
+++ bionumpy/sequence/count_encoded.py
@@ -6,14 +6,25 @@ from ..util.typing import EncodedArrayLike
 from ..encoded_array import EncodedArray
 
 
-@dataclasses.dataclass
 class EncodedCounts:
     alphabet: list
     counts: np.ndarray
     row_names: list = None
 
+    def __init__(self, alphabet, counts, row_names=None):
+        self.counts = counts
+        self.alphabet = alphabet
+        self.row_names = row_names
+
     def __str__(self):
         return "\n".join(f"{c}: {n}" for c, n in zip(self.alphabet, self.counts.T))
+
+    def __eq__(self, other):
+        if self.alphabet != other.alphabet:
+            return False
+        if not np.all(self.counts == other.counts):
+            return False
+        return True
 
     def __getitem__(self, idx):
         return self.counts[..., self.alphabet.index(idx)]
@@ -24,7 +35,7 @@ class EncodedCounts:
         else:
             assert self.alphabet==other.alphabet
             o_counts = other.counts
-        return dataclasses.replace(self, counts=self.counts+o_counts)
+        return self.__class__(self.alphabet, self.counts+o_counts)
 
     def __radd__(self, other):
         if isinstance(other, Number):
@@ -32,10 +43,15 @@ class EncodedCounts:
         else:
             assert self.alphabet==other.alphabet
             o_counts = other.counts
-        return dataclasses.replace(self, counts=self.counts+o_counts)
+        return self.__class__(self.alphabet, self.counts+o_counts)
+    # return dataclasses.replace(self, counts=self.counts+o_counts)
 
     def get_count_for_label(self, label):
         return np.sum(self.counts[..., self.alphabet.index(l)] for l in label)
+
+    @property
+    def labels(self):
+        return self.alphabet
 
     @classmethod
     def vstack(cls, counts):
@@ -46,6 +62,14 @@ class EncodedCounts:
         if row_names is not None:
             ret.row_names = [count.row_names for count in counts]
         return ret
+
+    def most_common(self, n=None):
+        args = np.argsort(self.counts)[::-1]
+        if n is not None:
+            args = args[:n]
+        return self.__class__(
+            [self.alphabet[i] for i in args],
+            self.counts[args])
 
 
 def count_encoded(values: EncodedArrayLike, weights: ArrayLike = None, axis: int = -1) -> EncodedCounts:
