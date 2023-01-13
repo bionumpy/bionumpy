a0003f4b934b9b41cc5f8cca0f88573963343b62
Knut Rand <knutdrand@gmail.com>
2023-01-13 15:34:10 +0100

    peak calling in benchmarking

---
+++ benchmarks/scripts/simulate_chipseq.py
@@ -0,0 +1,24 @@
+from pyjaspar import jaspardb
+import bionumpy as bnp
+from bionumpy.sequence.position_weight_matrix import PWM
+from bionumpy.datatypes import ChromosomeSize
+from bionumpy.simulate.chipseq import simulate_chip_seq_reads, simulate_sequence, ChipSeqSimulationSettings
+jaspar_object = jaspardb(release="JASPAR2020")
+ctcf_motif = jaspar_object.fetch_motifs_by_name('CTCF')[0]
+motif = PWM.from_dict(ctcf_motif.pwm)
+chromosome_sizes = {"chr1": 1000,
+                    "chr2": 2000,
+                    "chr3": 3000}
+chrom_sizes = ChromosomeSize(list(chromosome_sizes.keys()),
+                             list(chromosome_sizes.values()))
+
+# motif = read_motif("example_data/MA0080.1.jaspar")
+settings = ChipSeqSimulationSettings(motif)
+sequences = {name: simulate_sequence("acgt", size) for name, size in chromosome_sizes.items()}
+multistream = bnp.MultiStream(chromosome_sizes, sequences=sequences)
+reads = simulate_chip_seq_reads(multistream.sequences, settings, multistream.sequence_names)
+with bnp.open(snakemake.output[1], 'w') as f:
+    f.write(reads)
+with bnp.open(snakemake.output[0], "w") as f:
+    f.write(chrom_sizes)
+        
