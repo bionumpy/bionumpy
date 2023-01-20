315bdbb07f8e0c1bdc48863be93f8e3a7d38ce21
Knut Rand <knutdrand@gmail.com>
2023-01-20 11:38:33 +0100

    Reuse bam tests

--- tests/test_bam.py
+++ tests/test_bam.py
@@ -1,14 +1,19 @@
 import bionumpy as bnp
 import pytest
+from bionumpy.io.bam import BamIntervalBuffer
 from bionumpy.alignments import alignment_to_interval
 
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_read():
     filename = "/home/knut/Sources/bionumpy/example_data/test.bam"
     f = bnp.open(filename)
     d = f.read()
     print(d)
-    # d = np.concatenate(list(f.read_chunks(1000)))
-    print(alignment_to_interval(d))
-    # assert False
+
+
+def test_read_intervals():
+    filename = "/home/knut/Sources/bionumpy/example_data/test.bam"
+    f = bnp.open(filename, buffer_type=BamIntervalBuffer)
+    d = f.read()
+    print(d)
