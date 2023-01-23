ff1c1c2ff29e11e287acb0468c865859e7bc89b3
Knut Rand <knutdrand@gmail.com>
2023-01-23 12:36:43 +0100

    relative path

--- tests/test_bam.py
+++ tests/test_bam.py
@@ -6,14 +6,14 @@ from bionumpy.alignments import alignment_to_interval
 
 #@pytest.mark.skip
 def test_read():
-    filename = "/home/knut/Sources/bionumpy/example_data/test.bam"
+    filename = "example_data/test.bam"
     f = bnp.open(filename)
     d = f.read()
     print(d)
 
 
 def test_read_intervals():
-    filename = "/home/knut/Sources/bionumpy/example_data/test.bam"
+    filename = "example_data/test.bam"
     f = bnp.open(filename, buffer_type=BamIntervalBuffer)
     d = f.read()
     print(d)
