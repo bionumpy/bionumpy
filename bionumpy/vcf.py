6cb0fa240372eb52cba6753d78aa1f0e0cc12389
dn070017 <dn070017@gmail.com>
2023-03-06 14:27:10 +0100

    modified vcf.py

--- bionumpy/vcf.py
+++ bionumpy/vcf.py
@@ -220,16 +220,3 @@ def parse_header(lines: str) -> VCFHeader:
             warnings.warn(message, RuntimeWarning)    
 
     return VCFHeader(**headers)
-
-
-#def parse_info_line(line):
-#    line = line.strip()
-#    assert line.startswith("##INFO=<")
-#    assert line.endswith(">")
-#    line = line[8:-1]
-#    parts = line.split(",")[:3]
-#    return dict(tuple(part.split("=")) for part in parts)
-
-#def read_header(file_obj):
-#    lines = []    
-
