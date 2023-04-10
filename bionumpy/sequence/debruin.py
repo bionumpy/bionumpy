44a12c62e3af4d384a95b64cb1b32eb00e10020d
Knut Rand <knutdrand@gmail.com>
2023-04-10 10:46:05 +0200

    debruijn graph init

--- bionumpy/sequence/debruin.py
+++ bionumpy/sequence/debruin.py
@@ -1,6 +1,7 @@
 from .kmers import get_kmers, KmerEncoding
 from ..encodings import DNAEncoding
 from ..encoded_array import as_encoded_array, EncodedArray
+from collections import defaultdict
 from typing import List, Dict
 
 
@@ -40,6 +41,26 @@ class DeBruijnGraph:
 
     def kmers(self):
         pass
+
+
+class ColoredDeBruijnGraph:
+    def __init__(self, color_dict, k):
+        self._color_dict = color_dict
+        self._kmer_encoding = KmerEncoding(DNAEncoding, k)
+
+    @classmethod
+    def from_sequences(cls, sequences, k):
+        color_dict = defaultdict(list)
+        kmers = get_kmers(as_encoded_array(sequences), k)
+        for i, row in enumerate(kmers.raw()):
+            for kmer in row:
+                color_dict[kmer].append(i)
+        return cls(color_dict, k)
+
+    def __getitem__(self, idx):
+        kmer = as_encoded_array(idx, self._kmer_encoding).raw()
+        print(kmer)
+        return self._color_dict[int(kmer)]
 
 
 class KallistoIndex:
--- tests/test_debruijn.py
+++ tests/test_debruijn.py
@@ -1,16 +1,11 @@
 import pytest
-from bionumpy.sequence.debruin import DeBruijnGraph
+from bionumpy.sequence.debruin import DeBruijnGraph, ColoredDeBruijnGraph
 from bionumpy.util.testing import assert_encoded_array_equal
 
-@pytest.fixture
-def sequences():
-    return ['acg', 'gtc']
 
 @pytest.fixture
 def sequences():
-    return ['acg', 'gtc']
-
-
+    return ['acg', 'cgtc']
 
 
 def test_create_debruijn(sequences):
@@ -26,6 +21,19 @@ def test_forward(sequences):
 def test_backward(sequences):
     graph = DeBruijnGraph.from_sequences(sequences, 2)
     assert graph.backward('tc') == ['GT']
+
+
+def test_create_colored_debruijn_graph(sequences):
+    graph = ColoredDeBruijnGraph.from_sequences(sequences, 2)
+    assert isinstance(graph, ColoredDeBruijnGraph)
+
+
+def test_colored_debruijn_graph(sequences):
+    graph = ColoredDeBruijnGraph.from_sequences(sequences, 2)
+    assert graph['ac'] == [0]
+    assert graph['tc'] == [1]
+    assert graph['cg'] == [0, 1]
+
 
 
 @pytest.mark.xfail
