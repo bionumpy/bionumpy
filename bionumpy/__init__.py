217cea916701ce50b61fc1530bb481d6b5831be5
BumpVersion Action <bumpversion@github-actions>
2023-04-18 10:47:10 +0000

    Bump version: 0.2.22 → 0.2.23

--- bionumpy/__init__.py
+++ bionumpy/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Knut Rand"""
 __email__ = "knutdrand@gmail.com"
-__version__ = '0.2.22'
+__version__ = '0.2.23'
 
 import npstructures as nps
 
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bionumpy/bionumpy',
-    version='0.2.22',
+    version='0.2.23',
     zip_safe=False,
 )
 
