1e7cc18f789187c0f3be06a885f94a50440ae77e
Knut Rand <knutdrand@gmail.com>
2023-09-29 16:42:17 +0200

    rmoved test

--- scripts/reverse_compliment_example.py
+++ scripts/reverse_compliment_example.py
@@ -9,6 +9,7 @@ def reverse_complement(input_filename, output_filename):
     with bnp.open(output_filename, "w", buffer_type=bnp.TwoLineFastaBuffer) as outfile:
         outfile.write(reversed_entries)
 
-def test():
+def _test():
+    assert False
     reverse_complement('benchmarks/results/dna_sequences/length150_nreads500000.fa', 'tmp.fa')
 
