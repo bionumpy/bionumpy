c1a3b77e5454373c5eae12d933a0253320f72a20
Knut Rand <knutdrand@gmail.com>
2024-02-23 11:54:19 +0100

    cleaning up documentation

--- scripts/fastq_filtering_example.py
+++ scripts/fastq_filtering_example.py
@@ -1,9 +1,8 @@
-import numpy as np
 import bionumpy as bnp
-from bionumpy import streamable
 
-def test(file="example_data/big.fq.gz", out_file="example_data/big_filtered.fq.gz"):
-    with bnp.open(out_file, 'w') as out_file:
+
+def test(file="example_data/big.fq.gz", out_filename="example_data/big_filtered.fq.gz"):
+    with bnp.open(out_filename, 'w') as out_file:
         for reads in bnp.open(file).read_chunks():
             min_quality_mask = reads.quality.min(axis=-1) > 1
             max_quality_mask = reads.quality.mean(axis=-1) > 10
--- scripts/gc_one_chr_example.py
+++ scripts/gc_one_chr_example.py
@@ -1,17 +1,20 @@
+from typing import Tuple
+
 import bionumpy as bnp
 from bionumpy.arithmetics import get_boolean_mask
 
 
-def analyze_within_chromosome(seq_fn, genes_fn):
-    chr1 = bnp.open(seq_fn).read()[0]
-    genes = bnp.open(genes_fn).read()
+def analyze_within_chromosome(seq_filename: str, genes_filename: str):
+    chr1 = bnp.open(seq_filename).read()[0]
+    genes = bnp.open(genes_filename).read()
     print("Gene-regions: ", genes)
     print("Fasta: ", chr1)
     gc_inside, gc_outside = gc_inside_and_outside(chr1.sequence, genes)
     print(f"GC inside: {gc_inside:.2f}, GC outside: {gc_outside:.2f}")
+    return gc_inside, gc_outside
 
 
-def gc_inside_and_outside(chr1_sequence, genes):
+def gc_inside_and_outside(chr1_sequence: bnp.EncodedArray, genes: bnp.Interval)-> Tuple[int, int]:
     gc_inside = get_gc_content(chr1_sequence, genes)
     gc_outside = get_gc_content(chr1_sequence, ~get_boolean_mask(genes, len(chr1_sequence)))
     return gc_inside, gc_outside
@@ -24,6 +27,6 @@ def get_gc_content(sequence, intervals):
     return gc_count / sum(nn_counts.values())
 
 if __name__ == '__main__':
-    analyze_within_chromosome("example_data/gc_test_onechr.fa", "example_data/gc_bedtest_onechr.bed")
-
+    result = analyze_within_chromosome("example_data/gc_test_onechr.fa", "example_data/gc_bedtest_onechr.bed")
+    assert result == (0.6, 0.2)
 
