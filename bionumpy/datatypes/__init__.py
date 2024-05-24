de55adf32d1b5c78fc6ce83e1deb938bd97aa990
Knut Rand <knutdrand@gmail.com>
2024-05-24 13:20:15 +0200

    clean

--- bionumpy/datatypes/__init__.py
+++ bionumpy/datatypes/__init__.py
@@ -155,26 +155,6 @@ class SNP(Variant):
     pass
 
 
-class SortedIntervals:
-    def __init__(self, data):
-        self.data = np.asanyarray(data)
-        assert data.shape[-1] == 2
-        assert len(data.shape) == 2
-
-        self.starts = self.data[..., 0]
-        self.ends = self.data[..., 1]
-
-    def in_intervals(self, position):
-        idx = np.minimum(
-            np.searchsorted(self.starts, position, side="left"), self.starts.size - 1
-        )
-        return (position >= self.starts[idx]) & (position < self.ends[idx])
-
-    @classmethod
-    def concatenate(cls, elements):
-        return cls(np.vstack([element.data for element in elements]))
-
-
 @bnpdataclass
 class SAMEntry:
     name: SequenceID
