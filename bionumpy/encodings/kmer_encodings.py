031c4057b62c2f257948b6cb1347338d04c58775
Knut Rand <knutdrand@gmail.com>
2024-05-24 13:30:20 +0200

    docs: kmer encoding

--- bionumpy/encodings/kmer_encodings.py
+++ bionumpy/encodings/kmer_encodings.py
@@ -1,3 +1,5 @@
+from typing import Union, List
+
 from . import AlphabetEncoding
 from ..encoded_array import Encoding
 from ..encoded_array import EncodedArray, EncodedRaggedArray
@@ -7,21 +9,38 @@ import numpy as np
 
 
 class KmerEncoding(Encoding):
+    """
+    Encodes kmers of a given length using an alphabet encoding.
+    """
+
     def __init__(self, alphabet_encoding: AlphabetEncoding, k: int):
         assert is_subclass_or_instance(alphabet_encoding, AlphabetEncoding), alphabet_encoding
         self._alphabet_encoding = alphabet_encoding
         self._k = k
 
     @property
-    def k(self):
+    def k(self) -> int:
         return self._k
 
-    def encode(self, data):
+    def encode(self, data: Union[str, list, EncodedRaggedArray]) -> Union[EncodedArray, EncodedRaggedArray]:
+        """
+        Encodes a string, list of strings or EncodedRaggedArray into an EncodedArray or
+        EncodedRaggedArray of hashed kmers.
+
+        Parameters
+        ----------
+        data: Union[str, list, EncodedRaggedArray]
+
+        Returns
+        -------
+        Union[EncodedArray, EncodedRaggedArray]
+
+        """
         if isinstance(data, str):
             assert len(data) == self.k
             letters = self._alphabet_encoding.encode(data).raw()
             return EncodedArray(
-                letters.dot(self._alphabet_encoding.alphabet_size**np.arange(self._k)),
+                letters.dot(self._alphabet_encoding.alphabet_size ** np.arange(self._k)),
                 self)
         if isinstance(data, (list, EncodedRaggedArray)):
             assert all(len(row) == self.k for row in data)
@@ -29,12 +48,11 @@ class KmerEncoding(Encoding):
             if isinstance(letters, RaggedArray):
                 letters = letters.to_numpy_array()
             return EncodedArray(
-                letters.dot(self._alphabet_encoding.alphabet_size**np.arange(self._k)),
+                letters.dot(self._alphabet_encoding.alphabet_size ** np.arange(self._k)),
                 self)
-        print(data, type(data))
         raise NotImplementedError
 
-    def to_string(self, kmer):
+    def to_string(self, kmer: int) -> str:
         """
         Returns a human-readable string representation
         of an encoded kmer.
@@ -45,15 +63,15 @@ class KmerEncoding(Encoding):
             tmp = (kmer >> (2 * np.arange(self._k))) & 3
         else:
             n = self._alphabet_encoding.alphabet_size
-            tmp = (kmer//n**np.arange(self._k)) % n
+            tmp = (kmer // n ** np.arange(self._k)) % n
 
         chars = EncodedArray(tmp, self._alphabet_encoding)
         kmer = "".join(chr(int(b)) for b in chars.encoding.decode(chars).raw())
         return kmer
 
-    def get_labels(self):
+    def get_labels(self) -> List[str]:
         assert self._k <= 8, "Only supported for k <= 5"
-        return [self.to_string(kmer) for kmer in range(self._alphabet_encoding.alphabet_size**self._k)]
+        return [self.to_string(kmer) for kmer in range(self._alphabet_encoding.alphabet_size ** self._k)]
 
     def __str__(self):
         return f"{self._k}merEncoding({self._alphabet_encoding})"
