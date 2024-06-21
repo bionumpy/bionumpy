8fb3afecf7218bed5c10ef0d969acc887896d376
BumpVersion Action <bumpversion@github-actions>
2024-06-21 11:08:45 +0000

    Bump version: 1.0.10 → 1.0.11

--- bionumpy/__init__.py
+++ bionumpy/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Knut Rand"""
 __email__ = "knutdrand@gmail.com"
-__version__ = '1.0.10'
+__version__ = '1.0.11'
 
 import npstructures as nps
 
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bionumpy/bionumpy',
-    version='1.0.10',
+    version='1.0.11',
     zip_safe=False,
     extras_require={'full': ['isal']}
 )
