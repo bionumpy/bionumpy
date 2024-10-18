8170feea5409ad8c3c8f5a28f566fea2ed86a778
Knut Rand <knutdrand@gmail.com>
2024-10-18 17:24:20 +0200

    docs: more advanced example

--- scripts/explorations/mem_mapping.py
+++ scripts/explorations/mem_mapping.py
@@ -17,15 +17,27 @@ if __name__ == '__main__':
         bnp.BaseEncoding)  # type: ignore
 
     print(encoded_array)
-
+    encoded_array[4] = 'x'
+    print(encoded_array)
     encoded_ragged_array = bnp.EncodedRaggedArray(
         encoded_array, lengths)  # type: ignore
-
     # Write data to the memory-mapped file
     print(encoded_ragged_array)
-
+    encoded_ragged_array[:, 0] = 'y'
+    print(encoded_ragged_array)
     # Flush changes to disk
     data.flush()
-
+    lengths.flush()
     # Clean up when done (the file will remain on disk)
     del data
+    del lengths
+
+    # Make sure it's persisted
+    data = np.memmap('data.dat', dtype=dtype, mode='r', shape=shape)
+    lengths = np.memmap('myfile.dat', dtype=np.int32, mode='r', shape=4)
+    encoded_array = bnp.EncodedArray(
+        data,
+        bnp.BaseEncoding)  # type: ignore
+    encoded_ragged_array = bnp.EncodedRaggedArray(
+        encoded_array, lengths)  # type: ignore
+    print(encoded_ragged_array)
